aa6aa64363b6324e986558d5c37ad9a88e6e9701
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2024-12-29 19:01:06 +0530

    -Add grid pattern animation

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import TestimonialCard from "./TestimonialCard";
 import image from "@/assets/image.png";
 import { StaticImageData } from "next/image"; // Import StaticImageData from next/image
+import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
+import { cn } from "@/lib/utils";
 
 // Define the type for a testimonial, allowing avatar to be either a string (URL) or StaticImageData
 interface Testimonial {
   rating: number;
   comment: string;
-  avatar: string | StaticImageData;  // Update here to allow both types
+  avatar: string | StaticImageData; // Update here to allow both types
 }
 
 // The testimonials array with the proper typing
@@ -16,7 +18,7 @@ const testimonials: Testimonial[] = [
     rating: 5,
     comment:
       "Srishti's attention to details and creativity transformed our project. Her designs are not only beautiful but also highly functional.",
-    avatar: image,  // StaticImageData type
+    avatar: image, // StaticImageData type
   },
   {
     rating: 5,
@@ -52,7 +54,17 @@ const testimonials: Testimonial[] = [
 
 const Testimonials: React.FC = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-5">
+    <div className="relative flex flex-col justify-center items-center gap-5">
+      <AnimatedGridPattern
+        numSquares={30}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-20%] h-[100%] w-full skew-y-12"
+        )}
+      />
       <div className="text-center mb-10">
         <h1 className="font-semibold text-4xl">What Clients Say</h1>
         <p className="max-w-xl mt-5">
