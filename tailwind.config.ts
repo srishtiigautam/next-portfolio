f960fc218ea860c8a6580817e51415ab8f8d6b68
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2024-12-29 16:20:24 +0530

    -Add Oleao font from google-fonts -Remove dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        name: ['Oleo Script Swash Caps', 'cursive']
+      }
     },
   },
   plugins: [],
