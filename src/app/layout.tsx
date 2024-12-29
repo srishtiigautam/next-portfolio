2f2ccaab2c311af264cab1578c8487652ba2a1d9
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2024-12-29 16:21:44 +0530

    -Remove crossorigin attribute from googleapis link in index.html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com"  />
         <link
           href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps:wght@400;700&display=swap"
           rel="stylesheet"
