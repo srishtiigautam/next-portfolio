767d4a0658b292faab9ec035dfd9a7c07cadd74d
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2024-12-31 15:16:16 +0530

    -Changed breakpoints of Header from 768px to 1080px (custom-lg in tailwind.config)

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import Button from "./Button.component";
@@ -25,7 +25,7 @@ const Header: React.FC = () => {
       </div>
 
       {/* Hamburger Icon */}
-      <div className="md:hidden">
+      <div className="custom-lg:hidden">
         <button
           onClick={toggleMenu}
           aria-label="Toggle navigation menu"
@@ -68,7 +68,7 @@ const Header: React.FC = () => {
       </div>
 
       {/* Desktop Navigation */}
-      <div className="hidden md:flex justify-between items-center gap-5 text-xl">
+      <div className="hidden custom-lg:flex justify-between items-center gap-5 text-xl">
         <nav className="flex gap-5">
           <NavItem to="/" label="About Me" />
           <NavItem to="/projects" label="Projects" />
@@ -80,12 +80,12 @@ const Header: React.FC = () => {
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="absolute top-full right-0 bg-white shadow-md rounded-lg p-4 z-50 flex flex-col items-center gap-2 md:hidden">
+        <div className="absolute top-full right-0 bg-white shadow-md rounded-lg p-4 z-50 flex flex-col items-center gap-2 custom-lg:hidden">
           <nav className="flex flex-col w-full">
-            <NavItem to="/" label="About Me" className="block md:px-4 py-2" />
-            <NavItem to="/projects" label="Projects" className="block md:px-4 py-2" />
-            <NavItem to="/uiuxdesign" label="UI/UX Design" className="block md:px-4 py-2" />
-            <NavItem to="/testimonials" label="Testimonials" className="block md:px-4 py-2" />
+            <NavItem to="/" label="About Me" className="block px-4 py-2" />
+            <NavItem to="/projects" label="Projects" className="block px-4 py-2" />
+            <NavItem to="/uiuxdesign" label="UI/UX Design" className="block px-4 py-2" />
+            <NavItem to="/testimonials" label="Testimonials" className="block px-4 py-2" />
           </nav>
           <Button label="View Projects" />
         </div>
