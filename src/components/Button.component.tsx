9f0a4f8a322d2b16c3cec0235008f2965d6212b9
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2025-01-06 21:06:55 +0530

    -Modify button component to accept link for redirection and functions

--- src/components/Button.component.tsx
+++ src/components/Button.component.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+"use client";
+
+import React from "react";
 
 interface ButtonProps {
   label: string;
   className?: string;
+  redirectUrl?: string; // Optional URL for redirection
+  onClick?: () => void; // Optional onClick handler for custom functionality
 }
 
-const Button: React.FC<ButtonProps> = ({ label, className = "bg-pink-600 text-white" }) => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  className = "bg-pink-600 text-white",
+  redirectUrl,
+  onClick,
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      onClick(); // Execute the custom function if provided
+    } else if (redirectUrl) {
+      window.open(redirectUrl, "_blank"); // Open the URL in a new tab
+    }
+  };
+
   return (
-    <button className={`py-2 px-4 rounded-md ${className}`}>
+    <button
+      className={`py-2 px-4 rounded-md ${className}`}
+      onClick={handleClick}
+    >
       {label}
     </button>
   );
