5a506cd029ebcda996208b07a5d15386f39fd8ee
srishtiigautam <95361333+srishtiigautam@users.noreply.github.com>
2025-01-20 01:11:19 +0530

    -Add Mail component in Social Media Links

--- src/components/SocialMediaLinks.component.tsx
+++ src/components/SocialMediaLinks.component.tsx
@@ -1,37 +1,34 @@
 import { FaLinkedin, FaGithubSquare, FaTwitterSquare } from "react-icons/fa";
+import Mail from "./Mail.component";
 
 const SocialMediaLinks = () => {
   return (
-    <div className="flex flex-col">
-      <div className="flex justify-between">
-        <a
-          href="https://www.linkedin.com/in/-srishtigautam"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-pink-600"
-        >
-          <FaLinkedin size={30} />
-        </a>
-        <a
-          href="https://github.com/srishtiigautam"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-pink-600"
-        >
-          <FaGithubSquare size={30} />
-        </a>
-        <a
-          href="https://x.com/gautam_sri50255"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-pink-600"
-        >
-          <FaTwitterSquare size={30} />
-        </a>
-      </div>
-      <span className="hover:text-pink-600 mt-1">
-        &#128231; srishti.gautam0423@gmail.com
-      </span>
+    <div className="flex justify-between items-center gap-5">
+      <a
+        href="https://www.linkedin.com/in/-srishtigautam"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-pink-600"
+      >
+        <FaLinkedin size={30} />
+      </a>
+      <a
+        href="https://github.com/srishtiigautam"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-pink-600"
+      >
+        <FaGithubSquare size={30} />
+      </a>
+      <a
+        href="https://x.com/gautam_sri50255"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-pink-600"
+      >
+        <FaTwitterSquare size={30} />
+      </a>
+      <Mail />
     </div>
   );
 };
